8725ed423728e9fd369ec48ea56643d6a06eb0b5
MazorKing_xD <98781362+StevenRivasReyes@users.noreply.github.com>
2023-07-19 21:53:49 -0400

    clase 3

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,32 +2,39 @@ import pygame
 import random
 from pygame.sprite import Sprite
 
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH, METEOR
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from game.components.bullets.bullet import Bullet
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
     Y_POS = 0
     X_POS_RANGE = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-    SPEED_ON_Y = 1
-    SPEED_ON_X = 10
     MOVES = { 0: 'left', 1: 'right' }
+    INITIAL_SHOOTING_TIME = 1000
+    FINAL_SHOOTING_TIME = 3000
 
-    def __init__(self):
-        self.image = ENEMY_1
+    def __init__(self, image, speed_on_x, speed_on_y):
+        self.image = image
         self.image = pygame.transform.scale(self.image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
         self.rect = self.image.get_rect(midtop = (random.choice(self.X_POS_RANGE), self.Y_POS))
         self.direction = self.MOVES[random.randint(0, 1)]
         self.movement_count = 0
         self.moves_before_change = random.randint(20, 50)
+        self.speed_on_x = speed_on_x
+        self.speed_on_y = speed_on_y
+        self.type = 'enemy'
+        current_time = pygame.time.get_ticks()
+        self.shooting_time = random.randint(current_time + self.INITIAL_SHOOTING_TIME, current_time + self.INITIAL_SHOOTING_TIME)
 
-    def update(self, enemies):
-        self.rect.y += self.SPEED_ON_Y
+    def update(self, enemies, game):
+        self.rect.y += self.speed_on_y
+        self.shoot(game.bullet_manager)
 
         if self.direction == self.MOVES[0]:
-            self.rect.x -= self.SPEED_ON_X
+            self.rect.x -= self.speed_on_x
         elif self.direction == self.MOVES[1]:
-            self.rect.x += self.SPEED_ON_X
+            self.rect.x += self.speed_on_x
 
         self.handle_direction()
 
@@ -48,21 +55,10 @@ class Enemy(Sprite):
         if (self.movement_count >= self.moves_before_change):
             self.movement_count = 0
 
-class Meteor(Sprite):
-    METEOR_WIDTH = 40
-    METEOR_HEIGHT = 60
-    Y_POS = 0
-    X_POS_RANGE = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-    SPEED_ON_Y = 1
-    SPEED_ON_X = 10
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
 
-    def __init__(self):
-        self.image = METEOR
-        self.image = pygame.transform.scale(self.image, (self.METEOR_WIDTH, self.METEOR_HEIGHT))
-        self.rect = self.image.get_rect(midtop = (random.choice(self.X_POS_RANGE), self.Y_POS))
-    
-    def update(self):
-        pass
-    
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(self.INITIAL_SHOOTING_TIME, self.FINAL_SHOOTING_TIME)
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,24 +1,28 @@
-import pygame
-from game.components.enemies.enemy import Enemy,Meteor
+import random
+
+from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_1, ENEMY_2, ENEMY_3
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
+        self.enemy_images = [ENEMY_1, ENEMY_2, ENEMY_3]
     
-    def update(self):
+    def update(self, game):
         self.add_enemy()
 
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
-            enemy = Enemy()
+        if len(self.enemies) < 2:
+            image = random.choice(self.enemy_images)
+            speed_on_x = random.randint(10, 20)
+            speed_on_y = random.randint(1, 5)
+
+            enemy = Enemy(image, speed_on_x, speed_on_y)
             self.enemies.append(enemy)
-        elif len(self.enemies) > 1:
-            meteor = Meteor()
-            self.enemies.append(meteor)
