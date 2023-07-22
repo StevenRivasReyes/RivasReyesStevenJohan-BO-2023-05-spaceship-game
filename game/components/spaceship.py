9da628e9d1e7214626ac13ff1ddd0fb031dda635
MazorKing_xD <98781362+StevenRivasReyes@users.noreply.github.com>
2023-07-21 22:14:33 -0400

    clase 5

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,64 +1,60 @@
 import pygame
-import random
 from pygame.sprite import Sprite
 
-from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_TYPE
 from game.components.bullets.bullet import Bullet
 
-class Enemy(Sprite):
-    ENEMY_WIDTH = 40
-    ENEMY_HEIGHT = 60
-    Y_POS = 0
-    X_POS_RANGE = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-    MOVES = { 0: 'left', 1: 'right' }
-    INITIAL_SHOOTING_TIME = 1000
-    FINAL_SHOOTING_TIME = 3000
+# la clase Spaceship va a heredar de la clase Sprite
+class Spaceship(Sprite):
+    SPACESHIP_WIDTH = 40
+    SPACESHIP_HEIGHT = 60
+    SPACESHIP_POS_X = SCREEN_WIDTH / 2
+    SPACESHIP_POS_Y = 500
 
-    def __init__(self, image, speed_on_x, speed_on_y):
-        self.image = image
-        self.image = pygame.transform.scale(self.image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
-        self.rect = self.image.get_rect(midtop = (random.choice(self.X_POS_RANGE), self.Y_POS))
-        self.direction = self.MOVES[random.randint(0, 1)]
-        self.movement_count = 0
-        self.moves_before_change = random.randint(20, 50)
-        self.speed_on_x = speed_on_x
-        self.speed_on_y = speed_on_y
-        self.type = 'enemy'
-        current_time = pygame.time.get_ticks()
-        self.shooting_time = random.randint(current_time + self.INITIAL_SHOOTING_TIME, current_time + self.INITIAL_SHOOTING_TIME)
+    def __init__(self):
+        self.image = SPACESHIP
+        self.image = pygame.transform.scale(self.image, (self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
+        self.rect = self.image.get_rect(midbottom = (self.SPACESHIP_POS_X, self.SPACESHIP_POS_Y))
+        self.type = 'player'
+        self.has_power_up = False
+        self.power_up_type = DEFAULT_TYPE
+        self.power_up_time_up = 0
 
-    def update(self, enemies, game):
-        self.rect.y += self.speed_on_y
-        self.shoot(game.bullet_manager)
+    def update(self, user_input, game):
+        if user_input[pygame.K_LEFT]:
+            self.move_left()
+        elif user_input[pygame.K_RIGHT]:
+            self.move_right()
+        elif user_input[pygame.K_UP] and self.rect.top > 300:
+            self.rect.y -= 10
+        elif user_input[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
+            self.rect.y += 10
 
-        if self.direction == self.MOVES[0]:
-            self.rect.x -= self.speed_on_x
-        elif self.direction == self.MOVES[1]:
-            self.rect.x += self.speed_on_x
-
-        self.handle_direction()
-
-        if self.rect.top > SCREEN_HEIGHT:
-            enemies.remove(self)
-
+        if user_input[pygame.K_SPACE]:
+            self.shoot(game.bullet_manager)
+    
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-    def handle_direction(self):
-        self.movement_count += 1
-
-        if (self.movement_count >= self.moves_before_change and self.direction == self.MOVES[1]) or self.rect.right >= SCREEN_WIDTH:
-            self.direction = self.MOVES[0]
-        elif self.movement_count >= self.moves_before_change and self.direction == self.MOVES[0] or self.rect.left <= 0:
-            self.direction = self.MOVES[1]
-        
-        if (self.movement_count >= self.moves_before_change):
-            self.movement_count = 0
+    def move_right(self):
+        self.rect.x += 10
+        if self.rect.right >= SCREEN_WIDTH:
+            self.rect.left = -self.SPACESHIP_WIDTH
+    
+    def move_left(self):
+        self.rect.x -= 10
+        if self.rect.left <= 0:
+            self.rect.right = SCREEN_WIDTH + self.SPACESHIP_WIDTH
 
     def shoot(self, bullet_manager):
-        current_time = pygame.time.get_ticks()
+        bullet = Bullet(self)
+        bullet_manager.add_bullet(bullet)
 
-        if self.shooting_time <= current_time:
-            bullet = Bullet(self)
-            bullet_manager.add_bullet(bullet)
-            self.shooting_time += random.randint(self.INITIAL_SHOOTING_TIME, self.FINAL_SHOOTING_TIME)
+    def set_image(self, image = SPACESHIP, size = (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)):
+        self.image = image
+        self.image = pygame.transform.scale(self.image, size)
+
+    def reset(self):
+        self.image = SPACESHIP
+        self.image = pygame.transform.scale(self.image, (self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
+        self.rect = self.image.get_rect(midbottom = (self.SPACESHIP_POS_X, self.SPACESHIP_POS_Y))
